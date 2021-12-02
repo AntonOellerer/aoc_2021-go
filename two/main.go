c2aab18e4744c0c08e091159de227c03677c6527
Anton Oellerer <a.oellerer@docu-tools.com>
2021-12-02 11:36:14 +0100

    Solve day 2 part 2

--- two/main.go
+++ two/main.go
@@ -17,21 +17,23 @@ func main() {
 	contentString := string(content)
 	commandString := strings.Split(contentString, "\n")
 	x := 0
-	y := 0
+	depth := 0
+	aim := 0
 	for _, commandTuple := range commandString {
 		commandTuple := strings.Split(commandTuple, " ")
 		command := commandTuple[0]
-		moves, err := strconv.Atoi(commandTuple[1])
+		units, err := strconv.Atoi(commandTuple[1])
 		if err != nil {
 			log.Fatal(err)
 		}
 		if command == "forward" {
-			x += moves
+			x += units
+			depth += aim * units
 		} else if command == "down" {
-			y += moves
+			aim += units
 		} else if command == "up" {
-			y -= moves
+			aim -= units
 		}
 	}
-	fmt.Printf("%d \n", x*y)
+	fmt.Printf("%d \n", x*depth)
 }
